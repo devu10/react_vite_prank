ae592e7413f833f8f499f51c311ce98d97efad45
Devu <devudevu10@gmail.com>
2024-06-17 22:16:20 +1000

    html

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import "./App.css";
 
-function App() {
-  return <>Hello</>;
-}
+const App = () => {
+  return (
+    <>
+      <div className="wraper flex-center">
+        <div className="calculator">
+          <div className="d comic-neue-regular"></div>
+          <div className="ac cbtn">AC</div>
+          <div className="c cbtn">C</div>
+          <div className="per cbtn">%</div>
+          <div className="divide cbtn">/</div>
+          <div className="seven cbtn">7</div>
+          <div className="eight cbtn">8</div>
+          <div className="nine cbtn">9</div>
+          <div className="mul cbtn">*</div>
+          <div className="four cbtn">4</div>
+          <div className="five cbtn">5</div>
+          <div className="six cbtn">6</div>
+          <div className="minus cbtn">-</div>
+          <div className="one cbtn">1</div>
+          <div className="two cbtn">2</div>
+          <div className="three cbtn">3</div>
+          <div className="plus cbtn">+</div>
+          <div className="zero cbtn">0</div>
+          <div className="dot cbtn">.</div>
+          <div className="equal cbtn">=</div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default App;
